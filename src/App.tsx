97359fd7dc838f88edfbf12c2675651bc5305eb4
gia-grigalashvili <grigalagia07@gmail.com>
2024-05-29 22:36:44 +0400

    made Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import styled from "styled-components";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return <Conteiner></Conteiner>;
 }
-
+const Conteiner = styled.div`
+  width: 540px;
+  height: 332px;
+  border-radius: 15px;
+  background: #313a48;
+  box-shadow: 30px 50px 80px 0px rgba(0, 0, 0, 0.1);
+`;
 export default App;
